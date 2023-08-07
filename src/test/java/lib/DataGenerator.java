573ac26377590550fd68f26ea6afc0e24e80c20b
Артем <Karverx@gmail.com>
2023-08-08 01:08:46 +0300

    add tests - обновлен класс Assertions в директории lib/ - создан класс DataGenerator в директории lib/ - создан тест UserRegisterTest в директории tests/ добавлены тесты: testCreateUserWithExistingEmail testCreateUserSuccessfully

---
+++ src/test/java/lib/DataGenerator.java
@@ -0,0 +1,11 @@
+package lib;
+
+import java.text.SimpleDateFormat;
+
+
+public class DataGenerator {
+    public static String getRandomEmail() {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
+        return "learnqa" + timeStamp + "@example.com";
+    }
+}
